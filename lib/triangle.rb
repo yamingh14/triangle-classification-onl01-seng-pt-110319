06813dc7226c1eb3038299a4e1baacacb5c98014
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-30 17:47:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -57,7 +57,7 @@ class Triangle
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
-      elsif (@side_1 == @side_2) && (@side_2 == @side_3) && (@side_1 == @side_3)
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
